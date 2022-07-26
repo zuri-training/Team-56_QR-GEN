0f5c17e804426281dd201363de802ae1c2446bc5
imdadadelabou0 <imdadadelabou0@gmail.com>
2022-07-27 00:27:57 +0100

    [feat] manage signup route

--- qrSwift/auth/views.py
+++ qrSwift/auth/views.py
@@ -2,9 +2,18 @@ from django.shortcuts import render
 from .serializers import MyTokenObtainPairSerializer
 from rest_framework.permissions import AllowAny
 from rest_framework_simplejwt.views import TokenObtainPairView
+from django.contrib.auth.models import User
+from .serializers import RegisterSerializer
+from rest_framework import generics
 
 # Create your views here.
 
 class MyObtainTokenPairView(TokenObtainPairView):
     permission_classes = (AllowAny,)
     serializer_class = MyTokenObtainPairSerializer
+
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    permission_classes = (AllowAny,)
+    serializer_class=RegisterSerializer
