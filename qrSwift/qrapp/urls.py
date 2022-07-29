93bf7bfdc0fa0064c350dc6dc96ac30f920d6943
imdadadelabou0 <imdadadelabou0@gmail.com>
2022-07-29 10:54:54 +0100

    [feat] create route and controller for generationg code qr

--- qrSwift/qrapp/urls.py
+++ qrSwift/qrapp/urls.py
@@ -6,5 +6,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("create-qr/", views.create_qr)
     # path("main/", views.main, name="main"),
 ]
--- qrSwift/qrapp/views.py
+++ qrSwift/qrapp/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+
 from django.http import HttpResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
 
 # Create your views here.
 
@@ -7,3 +10,17 @@ from django.http import HttpResponse
 # testing initial setup
 def index(request):
     return HttpResponse("This is the home page!")
+
+@api_view(['POST'])
+def create_qr(request):
+
+    if not 'content' in request.data or not request.data["content"]:
+        return Response({"error": "Qr content is required"} , status=status.HTTP_400_BAD_REQUEST)
+    if not 'userId' in request.data:
+        return Response({"error": "userId is required"} , status=status.HTTP_400_BAD_REQUEST)
+
+    # Generate the qr code with content
+
+    qrContent = request.data["content"]
+    userId = request.data["userId"]
+    return Response({"content": qrContent, "userId": userId})
