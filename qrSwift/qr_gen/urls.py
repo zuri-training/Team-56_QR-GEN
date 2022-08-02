5c0d78b8788b836c7356403a7d30a6b183b73c16
imdadadelabou0 <imdadadelabou0@gmail.com>
2022-08-02 16:39:48 +0100

    [feat] creating delete route

--- qrSwift/qr_gen/urls.py
+++ qrSwift/qr_gen/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("create-qr/", views.create_qr)
+    path("create-qr/", views.create_qr),
+    path("delete-qr/<int:id>", views.delete_qr)
 ]
