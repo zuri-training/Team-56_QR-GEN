7d4d8b09efedd486789beea1a2be200219fcff6d
imdadadelabou0 <imdadadelabou0@gmail.com>
2022-08-04 17:36:58 +0100

    review code and add now we can generate qr code for card (contact)

--- qrSwift/qr_gen/views.py
+++ qrSwift/qr_gen/views.py
@@ -2,9 +2,9 @@ from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
-
+from rest_framework.renderers import JSONRenderer
 from .models import Qr
-from .serializers import QrSerializers
+from .serializers import  QrMeCardSerializers, QrSerializersText, QrWifiSerializers
 import json
 # Create your views here.
 
@@ -15,15 +15,36 @@ def create_qr(request):
     if not 'userId' in request.data:
         return Response({"error": "userId is required"}, status=status.HTTP_400_BAD_REQUEST)
     # Generate the qr code with content
-    
-    serializer = QrSerializers(data=request.data)
-    if serializer.is_valid():
-        qr_code = serializer.save()
-        data = Qr.objects.get(id=int(str(qr_code)))
 
-        print(serializer.data)
-        return Response({"res": "Qr code  created", "qr": {"id": data.id, "url": data.qr_code.url, "content": data.content, "name": data.name}}, status=status.HTTP_201_CREATED)
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    if not 'qr_type' in request.data:
+        return Response({"error": "qr_type is required"}, status=status.HTTP_400_BAD_REQUEST)
+        
+    qr_type = request.data["qr_type"]
+    
+    if qr_type == "CARD":
+        serializer = QrMeCardSerializers(data=request.data)
+        if serializer.is_valid():
+            qr_code = serializer.save()
+            json = QrMeCardSerializers(qr_code).data
+
+            return Response({"res": "Qr code creared", "qr": json}, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    elif qr_type == "WIFI":
+        serializer = QrWifiSerializers(data=request.data)
+        if serializer.is_valid():
+            qr_code = serializer.save()
+            json = QrWifiSerializers(qr_code).data
+
+            return Response({"res": "Qr code  created", "qr": json}, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    elif qr_type == "URL":
+        serializer = QrSerializersText(data=request.data)
+        if serializer.is_valid():
+            qr_code = serializer.save()
+            json = QrSerializersText(qr_code).data
+            
+            return Response({"res": "Qr code  created", "qr": json}, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 @api_view(['GET','DELETE'])
